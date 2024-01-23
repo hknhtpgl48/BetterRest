7d8d478e125f0f4d9d7a90cdbbc7a2ca0f91d764
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-01-23 14:15:10 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hakan Hatipoğlu on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
